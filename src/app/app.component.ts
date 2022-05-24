da440a0cc15a06898fce9dee0cc2e97dc78c0d83
CarlosHLeite <carlosleiteh@gmail.com>
2022-05-23 23:18:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+//importando dados
+import { dados } from './arquivo';
+
+//Criando component
 @Component({
   selector: 'app-root',
+  //apontando para o template html
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
+
+//Exportando dados
 export class AppComponent {
-  title = 'project';
+  dados = dados;
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,33 @@
+//DAdos
+export const dados = [
+  {
+    nome: 'Carlos',
+    ru: 3530269,
+    curso: 'BACHARELADO EM ENGENHARIA AGRONÔMICA',
+    data_de_nascimento: '13/03/1986',
+  },
+  {
+    nome: 'Julienne',
+    ru: 2777427,
+    curso: 'TECNOLOGIA EM AGRICULTURA',
+    data_de_nascimento: '15/06/2002',
+  },
+  {
+    nome: 'Charlie',
+    ru: 7834247,
+    curso: 'TECNOLOGIA EM AGRONOMIA',
+    data_de_nascimento: '10/10/1987',
+  },
+  {
+    nome: 'Julie',
+    ru: 129747,
+    curso: 'TECNOLOGIA EM GESTÃO DE RECURSOS HUMANOS',
+    data_de_nascimento: '04/09/2005',
+  },
+  {
+    nome: 'Alinne',
+    ru: 438203,
+    curso: 'BACHARELADO EM ARTES VISUAIS',
+    data_de_nascimento: '14/09/2021',
+  },
+];
